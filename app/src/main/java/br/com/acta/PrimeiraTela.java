9c3d094e3725cbc960d55b9653166e25fc1dd350
Lucas Almeida Da Costa <lucascosta-ieg@instituto.ad>
2026-06-10 16:13:12 -0300

    feat/Java para passar de tela

--- app/src/main/java/br/com/acta/PrimeiraTela.java
+++ app/src/main/java/br/com/acta/PrimeiraTela.java
@@ -1,9 +1,11 @@
 package br.com.acta;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.MotionLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -15,10 +17,28 @@ public class PrimeiraTela extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.btnComecar), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        MotionLayout motionLayoutButton = findViewById(R.id.btnComecar);
+        motionLayoutButton.setTransitionListener(new MotionLayout.TransitionListener() {
+            @Override
+            public void onTransitionStarted(MotionLayout motionLayout, int startId, int endId) {
+            }
+            @Override
+            public void onTransitionChange(MotionLayout motionLayout, int startId, int endId, float progress) {
+            }
+            public void onTransitionCompleted(MotionLayout motionLayout, int currentId) {
+                if (currentId == R.id.end) {
+                    Intent intent = new Intent(PrimeiraTela.this, ProximaTelaActivity.class);
+                    startActivity(intent);
+                }
+            }
+            @Override
+            public void onTransitionTrigger(MotionLayout motionLayout, int triggerId, boolean positive, float progress) {
+            }
         });
     }
 }
