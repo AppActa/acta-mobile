5f2c67730e4ab1f210e5e8451529f84fa7aceea7
LucasAlmeida2212 <lucas.almeidacosta09@gmail.com>
2026-08-20 21:48:06 -0300

    feat/ciclo

---
+++ app/src/main/java/br/com/acta/Model/Ciclo.java
@@ -0,0 +1,30 @@
+package br.com.acta.Model;
+
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.List;
+
+public class Ciclo {
+    private Long id;
+
+    private String titulo;
+
+    private String descricao;
+
+    private StatusCiclo status;
+
+    private LocalDate dataInicio;
+
+    private LocalDate dataEstimadaFim;
+    private LocalDate dataFimReal;
+
+    private Long idEmpresa;
+
+    private Long idGestor;
+
+    private List<UsuarioCiclo> colaboradores;
+
+    private OffsetDateTime criadoEm;
+    private OffsetDateTime atualizadoEm;
+
+}
---
+++ app/src/main/java/br/com/acta/Model/Enum/StatusCiclo.java
@@ -0,0 +1,5 @@
+package br.com.acta.Model.Enum;
+
+public enum StatusCiclo {
+    PLANEJAMENTO, EXECUCAO, VERIFICACAO, PADRONIZACAO, CONCLUIDO, CANCELADO, PAUSADO;
+}
