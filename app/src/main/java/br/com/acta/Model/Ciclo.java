7ee15c850a0d5cf7fe30f0c7cadf0c772c5bf033
LucasAlmeida2212 <lucas.almeidacosta09@gmail.com>
2026-08-21 21:21:00 -0300

    fix/adicionando anotações e import

--- app/src/main/java/br/com/acta/Model/Ciclo.java
+++ app/src/main/java/br/com/acta/Model/Ciclo.java
@@ -5,7 +5,15 @@ import java.time.OffsetDateTime;
 import java.util.List;
 
 import br.com.acta.Model.Enum.StatusCiclo;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Ciclo {
     private Long id;
 
--- app/src/main/java/br/com/acta/Model/UsuarioTreinamento.java
+++ app/src/main/java/br/com/acta/Model/UsuarioTreinamento.java
@@ -2,6 +2,16 @@ package br.com.acta.Model;
 
 import java.time.OffsetDateTime;
 
+import br.com.acta.Model.Enum.StatusTreinamento;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class UsuarioTreinamento {
     private Long idUsuario;
 
