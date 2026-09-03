bfc72524aeb1baec58f101e6956683518d2f0828
Lucas Almeida Da Costa <lucascosta-ieg@instituto.ad>
2026-09-02 21:49:00 -0300

    feat/Me model

---
+++ app/src/main/java/br/com/acta/Api/MeApi.java
@@ -0,0 +1,10 @@
+package br.com.acta.Api;
+
+import br.com.acta.Model.Me;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface MeApi {
+
+
+}
---
+++ app/src/main/java/br/com/acta/Model/Me.java
@@ -0,0 +1,23 @@
+package br.com.acta.Model;
+
+public class Me {
+    private String firebaseUid;
+
+    private Long idUsuario;
+
+    private Long idEmpresa;
+
+    private Long idColaborador;
+
+    private String nome;
+
+    private String email;
+
+    private String nomeEmpresa;
+
+    private String tipo;
+
+    private boolean permissaoGestor;
+
+    private String status;
+}
