b8ac28af2707f38f0aa7435f0c352340203c3457
Lucas Almeida Da Costa <lucascosta-ieg@instituto.ad>
2026-09-08 08:31:15 -0300

    fix/imports

--- app/src/main/java/br/com/acta/Services/MeService.java
+++ app/src/main/java/br/com/acta/Services/MeService.java
@@ -2,16 +2,9 @@ package br.com.acta.Services;
 
 import com.google.gson.Gson;
 
-import java.io.IOException;
-import java.util.List;
-
 import br.com.acta.Api.MeApi;
 import br.com.acta.Client.RepositoryCallback;
 import br.com.acta.Model.Me;
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MeService {
     private final MeApi api;
